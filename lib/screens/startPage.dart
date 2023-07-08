a4928570cf358c0ce6e2dc3732e6ea0bce255571
nourmamoun <nourmamoun@gmail.com>
2023-07-08 15:44:27 +0300

    third

--- lib/screens/startPage.dart
+++ lib/screens/startPage.dart
@@ -15,64 +15,67 @@ class StartPage extends StatelessWidget {
             backgroundColor: Colors.white,
 
         body: SafeArea(
-      child: Column(
-        
-        children: [
-          Container(
-              padding: const EdgeInsets.only(
-                  left: 46, top: 20, right: 41, bottom: 9),
-              child: Image.asset(
-                altruixLogoPath,
-                width: 303,
-                height: 109,
-              )),
-          Padding(
-            padding: const EdgeInsets.only(right: 44, top: 13, left: 11),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  "WELCOME ...",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Color(kThemeColor),
-                    fontFamily: kFontFamilyGraduate,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          
+          children: [
+            Container(
+                padding: const EdgeInsets.only(
+                    left: 46, top: 20, right: 41, bottom: 9),
+                child: Image.asset(
+                  altruixLogoPath,
+                  width: 303,
+                  height: 109,
+                )),
+            Padding(
+              padding: const EdgeInsets.only(right: 44, top: 13, left: 11),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "WELCOME ...",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(kThemeColor),
+                      fontFamily: kFontFamilyGraduate,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  'here is your new ',
-                  style: TextStyle(
-                    fontFamily: kFontFamilyGraduate,
-                    fontSize: 25,
-                    color: Color(kThemeColor),
+                  const SizedBox(
+                    height: 5,
                   ),
-                ),
-                const Text(
-                  'friend .. luka',
-                  style: TextStyle(
-                    fontFamily: kFontFamilyGraduate,
-                    fontSize: 25,
-                    color: Color(kThemeColor),
+                  const Text(
+                    'here is your new ',
+                    style: TextStyle(
+                      fontFamily: kFontFamilyGraduate,
+                      fontSize: 25,
+                      color: Color(kThemeColor),
+                    ),
                   ),
-                ),
-                Image.asset(kRobotImagePath,),
+                  const Text(
+                    'friend .. luka',
+                    style: TextStyle(
+                      fontFamily: kFontFamilyGraduate,
+                      fontSize: 25,
+                      color: Color(kThemeColor),
+                    ),
+                  ),
+                  Image.asset(kRobotImagePath,),
+                  
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              
+              children:  const[
+               ButtonWidget(buttonText: 'get started'),
+              ButtonWidget2(buttonText2: 'who we are')
                 
               ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            
-            children:  const[
-             ButtonWidget(buttonText: 'get started'),
-            ButtonWidget2(buttonText2: 'who we are')
-              
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     ));
   }

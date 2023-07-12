db108b5bc48ecbb0f30f0c8653ad323bb3b1ac85
nourmamoun <nourmamoun@gmail.com>
2023-07-12 16:11:12 +0300

    edit

--- lib/screens/weeklyFocusedActivity.dart
+++ lib/screens/weeklyFocusedActivity.dart
@@ -3,9 +3,11 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:luka_robot/constants.dart';
 import 'package:luka_robot/widgets/BackButton.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 
 class WeeklyFocusedActivity extends StatefulWidget {
@@ -19,6 +21,17 @@ class _WeeklyFocusedActivityState extends State<WeeklyFocusedActivity> {
   Query dbRef = FirebaseDatabase.instance
       .ref()
       .child('1TqtoCTXI7Xg5h3j38HeEbI5wz-NJOueMs0b8AYeQ1E8/Sheet1');
+
+  bool _isLoading=true;
+    void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      setState(() {
+        _isLoading=false;
+      });
+    });
+  }
 
   Widget TableRowContainer({required Map data}) {
     return Container(
@@ -70,88 +83,94 @@ class _WeeklyFocusedActivityState extends State<WeeklyFocusedActivity> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BackButtonWidget(imagePath: kBackButtonImage),
-            const Padding(
-              padding: EdgeInsets.only(left: 20, right: 71, top: 20),
-              child: Text(
-                'WEEKLY FOCUS REPORT',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: kFontFamilyGraduate,
-                    color: Color(kThemeColor),
-                    fontWeight: FontWeight.w400),
+    return ModalProgressHUD(
+      color: Color(kThemeColor),
+      inAsyncCall: _isLoading,
+
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BackButtonWidget(imagePath: kBackButtonImage),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 71, top: 20),
+                child: Text(
+                  'WEEKLY FOCUS REPORT',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: kFontFamilyGraduate,
+                      color: Color(kThemeColor),
+                      fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Table(
-              columnWidths: {
-                0: FixedColumnWidth(245),
-              },
-              border: TableBorder.all(
-                  width: 1.0,
-                  color: const Color(0xffE8ECF4),
-                  borderRadius: BorderRadius.circular(5)),
-              children: [
-                TableRow(
-                  
-                    decoration:
-                        BoxDecoration(color: Colors.white, boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 8,
-                      )
-                    ]),
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('TIME',
+              const SizedBox(
+                height: 50,
+              ),
+              Table(
+                columnWidths: {
+                  0: FixedColumnWidth(245),
+                },
+                border: TableBorder.all(
+                    width: 1.0,
+                    color: const Color(0xffE8ECF4),
+                    borderRadius: BorderRadius.circular(5)),
+                children: [
+                  TableRow(
+                    
+                      decoration:
+                          BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 3,
+                          blurRadius: 8,
+                        )
+                      ]),
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('TIME',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: kFontFamilyUbranist,
+                                  fontWeight: FontWeight.w700),
+                              textAlign: TextAlign.center),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'ACTIVITY',
                             style: TextStyle(
                                 fontSize: 16,
                                 fontFamily: kFontFamilyUbranist,
                                 fontWeight: FontWeight.w700),
-                            textAlign: TextAlign.center),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'ACTIVITY',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: kFontFamilyUbranist,
-                              fontWeight: FontWeight.w700),
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            Expanded(
-              child: Container(
-                child: FirebaseAnimatedList(
-                    query: dbRef,
-                     itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
-                        Map dataInfo = snapshot.value as Map;
-                      
-                        dataInfo["key"] = snapshot.key;
-                      
-                        return TableRowContainer(data: dataInfo);
-                     }),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ]),
+                ],
               ),
-            ),
-          ],
-        ),
-      )),
+              Expanded(
+                child: Container(
+                  child: FirebaseAnimatedList(
+                      query: dbRef,
+                       itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
+                        
+                          Map dataInfo = snapshot.value as Map;
+                        
+                          dataInfo["key"] = snapshot.key;
+                          return TableRowContainer(data: dataInfo);
+
+                       }),
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_database
+import modal_progress_hud_nsn
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
 }
